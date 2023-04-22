f6d46b61200a2bd16078b91197e828ef97b48824
Daniel <drekanzs@gmail.com>
2023-04-21 21:42:52 -0600

    Primer subida BTIENDAP completo

---
+++ src/api/completoarticulo/controllers/completoarticulo.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * completoarticulo controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::completoarticulo.completoarticulo');
---
+++ src/api/completoarticulo/routes/completoarticulo.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * completoarticulo router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::completoarticulo.completoarticulo');
